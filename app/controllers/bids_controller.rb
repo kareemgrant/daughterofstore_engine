bcad53fdc31fae0ab6a0991092f926afa9622186
Danny Garcia <dannygarcia.me@gmail.com>
2013-04-27 12:32:01 -0600

    refactor bids controller

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,14 +1,5 @@
 class BidsController < ApplicationController
 
-  # def create
-  #   if current_user
-  #     @bid = Bid.new(amount: params[:bid][:amount], auction_id: params[:auction_id], user_id: current_user.id)
-  #     @bid.save ? notice_message : alert_message
-  #   else
-  #     redirect_to login_path(return_to: auction_path(params[:auction_id])),
-  #     alert: "You must log in to bid."
-  #   end
-  # end
   def create
     if current_user
       @bid = Bid.new(amount: params[:bid][:amount], auction_id: params[:auction_id], user_id: current_user.id)
